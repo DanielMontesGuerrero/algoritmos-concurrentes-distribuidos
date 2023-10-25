0708e528a1b6565997a8b9773e0b560a4322b28f
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2023-10-25 17:43:50 -0600

    Add examples

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<signal.h>
+
+void handler(int);
+
+int main(void){
+	kill(SIGUSR1, handler);
+	while(1);
+	return 0;
+}
+
+void handler(int sig){
+	printf("Señal recibida: %d\n", sig);
+}
---
+++ pipes.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int i, error, fd[2];
+	char *buf = "a";
+	error = pipe(fd);
+	if(error < 0){
+		printf("Error wn la creacion del pipe\n");
+		return 1;
+	}
+	for(i = 1; ;i++){
+		write(fd[1], buf, 1);
+		printf("%d ", i);
+		fflush(stdout);
+	}
+	return 0;
+}
