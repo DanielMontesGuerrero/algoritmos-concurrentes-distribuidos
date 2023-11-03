7086f0d88a7e460f4f1c296200b8f497cd86d151
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2023-11-02 19:51:07 -0600

    Arreglar codigo productor-consumidor

--- practica1/procesos.c
+++ practica1/procesos.c
@@ -9,12 +9,20 @@
 
 int main(){
 	pid_t pid;
-	pid = fork();
-	if(pid == 0){
-		printf("Soy el hijo %d con padre %d\n", getpid(), getppid());
-	}
-	else{
-		printf("Soy el padre %d\n", getpid());
+	int i = 0;
+	for(i = 0; 1; i++){
+		pid = fork();
+		if(pid == 0){
+			printf("Soy el hijo %d con padre %d\n", getpid(), getppid());
+			while(1);
+		}
+		else if(pid < 0){
+			printf("El numero de procesos creados fue: %d\n", i);
+			break;
+		}
+		else{
+			printf("Soy el padre %d creando el proceso: %d\n", getpid(), i + 1);
+		}
 	}
 	return 0;
 }
