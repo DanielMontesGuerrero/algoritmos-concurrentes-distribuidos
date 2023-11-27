76211aedf166229772fd9c027f5aafa95eae33e7
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2023-11-27 17:52:37 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 import socket
 
+MAX_LEN = 1024
+
 port = 3000
 host = '148.204.63.78'
+#host = '127.0.0.1'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     s.listen()
+    print('Escuchando')
     while True:
         conn, addr = s.accept()
         with conn:
             while True:
                 data = conn.recv(MAX_LEN)
-                num = 2 * int(data)
-                num_str = str(num)
-                conn.sendall(num_str)
+                print('Mensaje:', data)
+                #num = 2 * int(data)
+                #num_str = str(num)
+                print('Envia:', data)
+                conn.sendall(data)
 
